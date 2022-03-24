9f91e6237b7c49e2077d5206dd3d6fbbb0845dc3
sreskala <sam.sagas@hotmail.com>
2022-03-23 20:23:05 -0400

    set up user authentication and login

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //connect DB
 dbConnect();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 //Define routes
