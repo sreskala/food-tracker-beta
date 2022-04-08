1cf28e28eac0abeea4be6fa09372ff1c7a3f6bbe
sreskala <sam.sagas@hotmail.com>
2022-04-07 21:24:41 -0400

    bare structure setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Navbar from "./components/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Layout />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <h1>Navbar</h1>
+        <Link to="/register">
+            <button>REGISTER</button>
+        </Link>
+        <Link to="/login">
+            <button>LOGIN</button>
+        </Link>
+    </div>
   )
 }
 
