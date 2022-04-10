228fe6001034e0dd27680dbab581289a6ade1301
sreskala <sam.sagas@hotmail.com>
2022-04-10 10:45:34 -0400

    adding register and login forms

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const { email, password } = formData;
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <div>Login</div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          value={email}
+          required
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          minLength="8"
+          value={password}
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
